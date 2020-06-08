6d06dab5f722f2661b27c3a17323b6df014ec4ff
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-06-07 21:20:29 -0400

    separate routes in another file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários')
-
-  response.json([
-    'Diego',
-    'Marcelo',
-    'Gabriel'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
