1c38b66120ee2b1fb92b1aadaf8b8ae9534fd389
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-06-08 00:06:16 -0400

    added route to list filtered points

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response} from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+      const points = await knex('points')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*');
+
+    return response.json(points);
+  }
+
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
@@ -30,11 +48,11 @@ class PointsController {
       uf,
       items
     } = request.body;
-  
+    
     const trx = await knex.transaction();
-
+    
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
@@ -43,9 +61,9 @@ class PointsController {
       city,
       uf
     };
-  
+    
     const insertedIds = await trx('points').insert(point);
-  
+    
     const point_id = insertedIds[0];
   
     const pointItems = items.map((item_id: number) => {
@@ -57,6 +75,8 @@ class PointsController {
   
     await trx('point_items').insert(pointItems);
   
+    await trx.commit();
+
     return response.json({
       id: point_id,
       ...point,
