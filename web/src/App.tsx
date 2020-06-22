e0bff13d6b184bf794a67ec5c4081fb5b04350f3
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-06-22 00:52:50 -0400

    added React State and test props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
+  const [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title={`Contador: ${counter}`} />
+      <h1>Conteúdo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
