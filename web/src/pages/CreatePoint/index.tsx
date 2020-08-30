fddab9321cd51ae44bc8eafe99e54717c313cf27
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-08-30 17:59:41 -0400

    load user initial location

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -31,9 +31,19 @@ const CreatePoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    })
+  }, [])
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -132,7 +142,7 @@ const CreatePoint = () => {
             <span>selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-22.2500367, -54.7961448]} zoom={15} onClick={handleMapClick}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
