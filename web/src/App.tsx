e93a422652b7c7b41f30461ae4496aad958101a7
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-08-30 12:29:36 -0400

    first HTML page built

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header title={`Contador: ${counter}`} />
-      <h1>Conteúdo da aplicação</h1>
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <Home />
     </div>
   );
 }
